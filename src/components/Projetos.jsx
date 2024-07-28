89b647eccee720de19b5fa25f8dd1bdee3a5d695
Lucas Ávila <147003894+LcAvila@users.noreply.github.com>
2024-07-28 12:16:38 -0300

    feat: add adjustment

--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -32,7 +32,7 @@ const Projetos = () => {
                         className="w-full max-w-xl lg:w-3/4">
                         <h6 className="mb-2 font-semibold">{project.title}</h6>
                         <p className="mb-4 font-neutral-400">{project.description}</p>
-                        <a href=""><motion.h6
+                        <a href="https://github.com/LcAvila/siteC" target="blank"><motion.h6
                             whileInView={{ opacity: 1, x: 0 }}
                             initial={{ opacity: 0, x: 100 }}
                             transition={{ duration: 1.5 }}
@@ -41,7 +41,7 @@ const Projetos = () => {
                         </motion.h6></a>
 
                         {project.technologies.map((tech, index) => (
-                            <span key={index} className="mr-5 rounded bg-neutral-900 px-5 py-2 text-sm font-medium text-purple-800">
+                            <span key={index} className="mr-2 rounded bg-neutral-900 px-5 py-1 text-sm font-medium text-purple-800">
                                 {tech}
                             </span>
                         ))}
