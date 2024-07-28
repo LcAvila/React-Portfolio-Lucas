6dc4f1c2980a5cd7d7440ae0f6346209440b901e
Lucas Ávila <147003894+LcAvila@users.noreply.github.com>
2024-07-28 12:32:02 -0300

    feat: add adjustment

--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -36,15 +36,15 @@ const Projetos = () => {
                             whileInView={{ opacity: 1, x: 0 }}
                             initial={{ opacity: 0, x: 100 }}
                             transition={{ duration: 1.5 }}
-                            className="text-center font-bold text-purple-600">
+                            className="my-5 text-center font-bold text-purple-600">
                             {project.botao}
                         </motion.h6></a>
-
                         {project.technologies.map((tech, index) => (
-                            <span key={index} className="p-4 mr-2 lg:w-1/2 rounded bg-neutral-900 px-5 py-1 text-sm font-medium text-purple-800">
+                            <span key={index} className="mr-4 rounded bg-neutral-900 px-5 py-1 text-sm font-medium text-purple-800">
                                 {tech}
                             </span>
                         ))}
+
                     </motion.div>
 
                 </div>
